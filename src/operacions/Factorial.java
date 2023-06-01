d0bda46ff6b35b4104855736794e5543a9f5e5a1
wuillem11 <garciaperezguillemins@gmail.com>
2023-06-01 20:35:13 +0200

    Factorial clase

---
+++ src/operacions/Factorial.java
@@ -0,0 +1,20 @@
+package operacions;
+
+public class Factorial {
+
+	public double factorialIteratiu(int n) {
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		if (n == 1 || n == 0) {
+			return 1;
+		}
+
+		int aux = 1;
+		for (int i = 2; i <= n; i++) {
+			aux *= i;
+		}
+		return aux;
+	}
+
+}
